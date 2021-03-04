d791f26016a36e3ada13c5c99207f82cc4e7ba4b
yizuo <yizuo.chen@ucla.edu>
2021-03-04 02:03:09 -0500

    New branch with only server-database

--- pythonserver/image_processing.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from PIL import Image
-import cv2
-
-def resize_image(in_img,shape=(299,299)):
-    img = cv2.resize(in_img, shape)
-    return img
-
--- tensorflow_server/run_servers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo docker run -p 8501:8501 \
-       --mount type=bind,source="/home/gerry/Documents/cs130/6/tensorflow_server/inception_server/",target=/models/inception_model -e MODEL_NAME=inception_model -t tensorflow/serving & \
-sudo docker run -p 8502:8501 \
-       --mount type=bind,source="/home/gerry/Documents/cs130/6/tensorflow_server/autoencoder_server/",target=/models/autoencoder_model -e MODEL_NAME=autoencoder_model -t tensorflow/serving
-
-
